c939d3e0d51039e59cbc867c142e07b34110272c
brittbarreto <43016971+brittbarreto@users.noreply.github.com>
2019-03-19 23:12:18 -0700

    Update question9.R

--- question9.R
+++ question9.R
@@ -7,7 +7,11 @@ library(readxl)
 #import the data
 loc <- read_excel("data/ca_ozone/location.xls")
 
-##this is a shorter and "cleaner" version of doing searching for the patterns and
-##would allow to easily re edit the "San|Santa" for future uses.
-match<- loc %>%  
-  filter(str_detect(`Site Name`, "^(San|Santa)"))
+#extract the Site Name column as a vector
+SiteNames <- pull(loc,"Site Name")
+
+#count the numbers of site containg "San" or "Santa" in thier name
+sum(str_count(SiteNames, "San|Santa"))
+
+##Adding this line to the end of your code would allow you to see the addresses with this string rather than just the number of how many contain the string.
+str_view_all(SiteNames, "San\\s|Santa", match = T)
